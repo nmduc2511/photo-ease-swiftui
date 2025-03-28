e9d6b82cc77128148f2a4d98f5350e92b543534c
Duc Nguyen <>
2025-03-28 20:35:02 +0700

    initial project

---
+++ photo-ease-swiftui/photo_ease_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_ease_swiftuiApp.swift
+//  photo-ease-swiftui
+//
+//  Created by Admin on 28/3/25.
+//
+
+import SwiftUI
+
+@main
+struct photo_ease_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
